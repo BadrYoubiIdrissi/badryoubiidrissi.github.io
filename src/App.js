b22cc540827f7d23ab31506b5633210dc67adb4a
Badr Youbi Idrissi <badr.youbi-idrissi@student.ecp.fr>
2019-03-15 15:23:42 +0100

    Added particles background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import particleParams from './assets/particles';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,20 +8,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header className="App-header panel">
+          <Particles className="background" params={particleParams}/>
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <div className="panel">
+        </div>
       </div>
     );
   }
