25c9a52c06c8a35c02f46d3dd607996b7fb077ab
Badr Youbi Idrissi <badryoubiidrissi@gmail.com>
2019-03-19 23:49:07 +0100

    Second panel

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,120 +1,11 @@
-var partJson = {
-  particles: {
-    number: {
-      value: 100,
-      density: {
-        enable: true,
-        value_area: 500
-      }
-    },
-    color: {
-      value: "#aaaaaa"
-    },
-    shape: {
-      type: "circle",
-      stroke: {
-        width: 0,
-        color: "#000000"
-      },
-      polygon: {
-        nb_sides: 5
-      },
-      image: {
-        src: "img/github.svg",
-        width: 100,
-        height: 100
-      }
-    },
-    opacity: {
-      value: 0.5,
-      random: false,
-      anim: {
-        enable: false,
-        speed: 1,
-        opacity_min: 0.1,
-        sync: false
-      }
-    },
-    size: {
-      value: 3,
-      random: true,
-      anim: {
-        enable: false,
-        speed: 40,
-        size_min: 0.1,
-        sync: false
-      }
-    },
-    line_linked: {
-      enable: true,
-      distance: 150,
-      color: "#999999",
-      opacity: 0.4,
-      width: 1
-    },
-    move: {
-      enable: true,
-      speed: 2,
-      direction: "bottom-right",
-      random: true,
-      straight: false,
-      out_mode: "out",
-      bounce: false,
-      attract: {
-        enable: false,
-        rotateX: 600,
-        rotateY: 1200
-      }
-    }
-  },
-  interactivity: {
-    detect_on: "canvas",
-    events: {
-      onhover: {
-        enable: false,
-        mode: "repulse"
-      },
-      onclick: {
-        enable: true,
-        mode: "push"
-      },
-      resize: true
-    },
-    modes: {
-      grab: {
-        distance: 400,
-        line_linked: {
-          opacity: 1
-        }
-      },
-      bubble: {
-        distance: 400,
-        size: 40,
-        duration: 2,
-        opacity: 8,
-        speed: 3
-      },
-      repulse: {
-        distance: 200,
-        duration: 0.4
-      },
-      push: {
-        particles_nb: 4
-      },
-      remove: {
-        particles_nb: 2
-      }
-    }
-  },
-  retina_detect: true
-};
-var jsonUri = "data:text/plain;base64," + window.btoa(JSON.stringify(partJson));
+particlesJS.load("header-particles", headerParticles);
 
-particlesJS.load("particles-js-1", jsonUri);
+$(window).on("load", function() {
+  var svgObject = document.getElementById("panel2_svg"),
+    svgDoc = svgObject.contentDocument,
+    neurones = svgDoc.getElementById("neurones");
+  console.log(neurones);
 
-$(function() {
-  // wait for document ready
-  // init
   var controller = new ScrollMagic.Controller({
     globalSceneOptions: {
       triggerHook: "onLeave"
@@ -127,11 +18,18 @@ $(function() {
   // create scene for every slide
   for (var i = 0; i < slides.length; i++) {
     new ScrollMagic.Scene({
-      triggerElement: slides[i]
+      triggerElement: slides[i],
+      duration: "20%"
     })
       .setPin(slides[i])
       .addTo(controller);
   }
+
+  var tl = new TimelineMax().from(neurones, 1, { color: "white" });
+  new ScrollMagic.Scene({ triggerElement: slides[1] })
+    .setTween(tl)
+    .addTo(controller);
+
   controller.scrollTo(function(newpos) {
     TweenMax.to(window, 0.5, { scrollTo: { y: newpos } });
   });
