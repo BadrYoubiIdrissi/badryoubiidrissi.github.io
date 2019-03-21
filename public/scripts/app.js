4f39ccffd32c5078154914ceaa11f51610783072
Badr Youbi Idrissi <badryoubiidrissi@gmail.com>
2019-03-21 12:20:17 +0100

    Animation half working

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,34 +1,57 @@
 particlesJS.load("header-particles", headerParticles);
 
+function resetPath(path){
+  var l = path.getTotalLength();
+  TweenMax.set(path, {strokeDasharray:l, strokeDashoffset:l});
+}
+
+function animateDraw(path, time=1){
+  var l = path.getTotalLength();
+  return TweenMax.to(path, time, {strokeDashoffset:0.0, onUpdate : function(){console.log(this)}});
+}
+
 $(window).on("load", function() {
   var svgObject = document.getElementById("panel2_svg"),
     svgDoc = svgObject.contentDocument,
-    neurones = svgDoc.getElementById("neurones");
-  console.log(neurones);
+    heart = svgDoc.getElementById("Heart"),
+    neurones = svgDoc.getElementsByClassName("neurone"),
+    synapses = svgDoc.getElementsByClassName("synapse"),
+    startSyn = svgDoc.getElementById("startSyn"),
+    endSyn = svgDoc.getElementById("endSyn");
+
+  [].forEach.call(synapses, resetPath);
 
   var controller = new ScrollMagic.Controller({
     globalSceneOptions: {
-      triggerHook: "onLeave"
+      triggerHook: "onCenter"
     }
   });
 
   // get all slides
   var slides = document.querySelectorAll(".panel");
 
-  // create scene for every slide
-  for (var i = 0; i < slides.length; i++) {
-    new ScrollMagic.Scene({
-      triggerElement: slides[i],
-      duration: "20%"
-    })
-      .setPin(slides[i])
-      .addTo(controller);
-  }
+  var tl = new TimelineMax()
+    .add(animateDraw(startSyn))
+    .add(animateDraw(endSyn))
+    .from(neurones, 1, { fill: "#104354" })
+    .add([].map.call(synapses, animateDraw))
+  //tween = TweenMax.to(neurones, 1, { fill: "#111" });
 
-  var tl = new TimelineMax().from(neurones, 1, { color: "white" });
-  new ScrollMagic.Scene({ triggerElement: slides[1] })
+  new ScrollMagic.Scene({
+    triggerElement: slides[1]
+  })
     .setTween(tl)
     .addTo(controller);
+
+  // // create scene for every slide
+  // for (var i = 0; i < slides.length; i++) {
+  //   new ScrollMagic.Scene({
+  //     triggerElement: slides[i]
+  //   })
+  //     .setPin(slides[i])
+  //     .addTo(controller);
+  // }
+  //
 
   controller.scrollTo(function(newpos) {
     TweenMax.to(window, 0.5, { scrollTo: { y: newpos } });
