d1f18512c1f8ef7e791a7e931bb23e2216ac77b9
Badr Youbi Idrissi <badryoubiidrissi@gmail.com>
2019-03-24 19:49:31 +0100

    summary done

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,26 +1,134 @@
 particlesJS.load("header-particles", headerParticles);
 
-function resetPath(path){
-  TweenMax.set(path, {strokeDasharray:10000, strokeDashoffset:10000});
+function animateDraw(path, time = 1) {
+  var l = path.getTotalLength();
+  var tl = new TimelineMax();
+  var num = Math.ceil(1+l);
+  tl.set(path, { strokeDasharray: num, strokeDashoffset: num , immediateRender:true});
+  tl.to(path, time, { strokeDashoffset: num - l });
+  return tl;
 }
 
-function animateDraw(path, time=1){
+function dashArr2String(dashArr) {
+  return dashArr.a + " " + dashArr.b + " " + dashArr.c + " " + dashArr.d;
+}
+
+function animateStrokeDashArray(path) {
+  var strokeArr = dashArr2String(this.target);
+  path.style.strokeDasharray = strokeArr;
+}
+
+function dashArray(a, b, c = false, d = false) {
+  c = c ? c : a;
+  d = d ? d : b;
+  return { a: a, b: b, c: c, d: d };
+}
+
+function animateDashArr(path, initial, final, time) {
+  return TweenMax.to(initial, time, final).eventCallback(
+    "onUpdate",
+    animateStrokeDashArray,
+    [path]
+  );
+}
+
+function animateChase(path, initialDashArray, finalDashArray, dashOffset, time) {
   var l = path.getTotalLength();
-  return TweenMax.to(path, time, {strokeDashoffset:10000-l});
+  var tl = new TimelineMax();
+  tl.set(path, { strokeDasharray: dashArr2String(initialDashArray), immediateRender:true });
+  tl.add(animateDashArr(path, initialDashArray, finalDashArray, time));
+  return tl.to(path, time, { strokeDashoffset: -l + dashOffset }, 0);
+}
+
+function animateFire(fire) {
+  var tl = new TimelineMax();
+  tl.set(fire, { transformOrigin: "50% 100%", immediateRender: true });
+  var list = [];
+  for (var el of fire) {
+    list.push(
+      TweenMax.fromTo(
+        el,
+        0.5,
+        { scaleY: 0.1, opacity: 0 },
+        { scaleY: 0.9 + 0.2 * Math.random(), opacity: 1, ease: Linear.easeIn }
+      )
+    );
+  }
+  var looptl = new TimelineMax({ repeat: -1, yoyo: true });
+  for (var i = 0; i <= 10; i++) {
+    var l = [];
+    for (var el of fire) {
+      l.push(
+        TweenMax.to(el, 0.3, {
+          scaleX: 0.9 + 0.15 * Math.random(),
+          scaleY: 0.9 + 0.25 * Math.random(),
+          ease: Linear.easeIn
+        })
+      );
+    }
+    looptl.add(l);
+  }
+  tl.add(list);
+  tl.add(looptl);
+  return tl;
+}
+
+function animateFire(fire) {
+  var tl = new TimelineMax();
+  tl.set(fire, { transformOrigin: "50% 100%", immediateRender: true });
+  var list = [];
+  for (var el of fire) {
+    list.push(
+      TweenMax.fromTo(
+        el,
+        0.5,
+        { scaleY: 0.1, opacity: 0 },
+        { scaleY: 0.9 + 0.2 * Math.random(), opacity: 1, ease: Linear.easeIn }
+      )
+    );
+  }
+  var looptl = new TimelineMax({ repeat: -1, yoyo: true });
+  for (var i = 0; i <= 10; i++) {
+    var l = [];
+    for (var el of fire) {
+      l.push(
+        TweenMax.to(el, 0.3, {
+          scaleX: 0.9 + 0.15 * Math.random(),
+          scaleY: 0.9 + 0.25 * Math.random(),
+          ease: Linear.easeIn
+        })
+      );
+    }
+    looptl.add(l);
+  }
+  tl.add(list);
+  tl.add(looptl);
+  return tl;
+}
+
+function mapAni(arr, animFunction, time){
+  var aniArr = [];
+  for (var e of arr){
+    aniArr.push(animFunction(e, time));
+  }
+  return aniArr
 }
 
 $(window).on("load", function() {
-  var svgObject = document.getElementById("panel2_svg"),
-    svgDoc = svgObject.contentDocument,
-    heart = svgDoc.getElementById("Heart"),
-    neurones = svgDoc.getElementsByClassName("neurone"),
-    synapses = svgDoc.getElementsByClassName("synapse"),
-    synapses1 = svgDoc.getElementsByClassName("synapse1"),
-    startSyn = svgDoc.getElementById("startSyn"),
-    endSyn = svgDoc.getElementById("endSyn");
-
-  [].forEach.call(synapses, resetPath);
-  [].forEach.call(synapses1, resetPath);
+  var leftSVG = document.getElementById("summary_anim_left").contentDocument,
+    neurones = leftSVG.getElementsByClassName("neurone"),
+    synapses = leftSVG.getElementsByClassName("synapse"),
+    synapses1 = leftSVG.getElementsByClassName("synapse1"),
+    main_syn = leftSVG.getElementById("main-synapse"),
+    rightSVG = document.getElementById("summary_anim_right").contentDocument,
+    icons = rightSVG.getElementById("icons"),
+    question = rightSVG.getElementById("question"),
+    question_mark = rightSVG.getElementById("question_mark")
+    leftBracket = rightSVG.getElementById("left-bracket"),
+    middle = rightSVG.getElementById("middle"),
+    rightBracket = rightSVG.getElementById("right-bracket"),
+    fire = rightSVG.getElementsByClassName("fire"),
+    secondary_syn = rightSVG.getElementById("secondary-synapse");
 
   var controller = new ScrollMagic.Controller({
     globalSceneOptions: {
@@ -32,33 +140,31 @@ $(window).on("load", function() {
   var slides = document.querySelectorAll(".panel");
 
   var tl = new TimelineMax()
-    .from(neurones, 1, { fill: "#104354" })
-    .add([].map.call(synapses, animateDraw))
-    .add([].map.call(synapses1, animateDraw))
-    .from(endSyn, 0.1, {strokeWidth:0})
-    .from(startSyn, 1, {attr : {y2:startSyn.y1.baseVal.value}})
-    .from(endSyn, 3, {attr : {y2:endSyn.y1.baseVal.value}})
-  //tween = TweenMax.to(neurones, 1, { fill: "#111" });
-
+    .add("origin", 0)
+    .from(neurones, 0.3, { fill: "#104354" }, "origin")
+    .add(mapAni(synapses, animateDraw, 0.5))
+    .add(mapAni(synapses1, animateDraw, 0.5))
+    .add("brain")
+    .add(animateDraw(main_syn, 2),"brain")
+    .add(animateDraw(secondary_syn, 2), "brain")
+    .add("end_syn")
+    .from(icons, 0.2, {opacity:0})
+    .add(animateChase(question,dashArray(1, 1000),dashArray(100, 1000),100, 1), "end_syn")
+    .from(question_mark, 0.3, {scale: 0})
+    .add(animateFire(fire), "end_syn")
+    .add(animateChase(leftBracket, dashArray(1, 1000), dashArray(65, 1000), 65, 1), "end_syn")
+    .add(animateChase(middle, dashArray(1, 1000), dashArray(47, 1000), 47, 1), "end_syn")
+    .add(animateChase(rightBracket, dashArray(1, 1000), dashArray(67, 1000), 67, 1), "end_syn");
   new ScrollMagic.Scene({
-    triggerElement: slides[1],
-    duration:"50%"
+    triggerElement: slides[1]
   })
     .setTween(tl)
     .addTo(controller);
 
-  // // create scene for every slide
-  // for (var i = 0; i < slides.length; i++) {
-  //   new ScrollMagic.Scene({
-  //     triggerElement: slides[i]
-  //   })
-  //     .setPin(slides[i])
-  //     .addTo(controller);
-  // }
-  //
+  
 
   controller.scrollTo(function(newpos) {
-    TweenMax.to(window, 1.5, { scrollTo: { y: newpos } , ease:"exp"});
+    TweenMax.to(window, 1.5, { scrollTo: { y: newpos }, ease: "exp" });
   });
   //  bind scroll to anchor links
   $(document).on("click", "a[href^='#']", function(e) {
